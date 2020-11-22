24f0c398076e1f9baa3b81c9dfe7be46e79d6941
Yunus Emre Gündoğmuş <32923721+yemregundogmus@users.noreply.github.com>
2020-11-22 23:20:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="API Library for SumAPI with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/summarify/sumAPI-Package",
+    url="https://github.com/summarify/sumapi",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
